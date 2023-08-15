fe68f3f69a8b388ba5414c509af8f0a363e72058
Noblelar <59696875+Noblelar@users.noreply.github.com>
2023-08-15 22:13:51 +0000

    Bend

--- index.js
+++ index.js
@@ -26,4 +26,7 @@ app.post("/authenticate", async (req, res) => {
 });
 
 
-app.listen(4000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
